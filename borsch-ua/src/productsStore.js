e167cf939f387dadfe15da171e3a59924503313d
Username <apkolesnyk94@gmail.com>
2022-11-15 22:11:37 -0500

    update

--- borsch-ua/src/productsStore.js
+++ borsch-ua/src/productsStore.js
@@ -3,33 +3,33 @@
 // pin: price_1M0pGZJjFKHFVjIv9mwLonjd
 
 // flag: price_1M0pHCJjFKHFVjIvPcUevcQh
-const productsArray = [
-{
-    id:"price_1M0pCwJjFKHFVjIvKVRB3xzH",
-    title: "Blackberry jam",
-    price: 4.99
-},
-{
-    id:"price_1M0pGZJjFKHFVjIv9mwLonjd",
-    title: "ukrainian pin",
-    price: 14.99
-},
-{
-    id:"price_1M0pHCJjFKHFVjIvPcUevcQh",
-    title: "Flag of Ukraine",
-    price: 10.99
-},
-]
+// const productsArray = [
+// {
+//     id:"price_1M0pCwJjFKHFVjIvKVRB3xzH",
+//     title: "Blackberry jam",
+//     price: 4.99
+// },
+// {
+//     id:"price_1M0pGZJjFKHFVjIv9mwLonjd",
+//     title: "ukrainian pin",
+//     price: 14.99
+// },
+// {
+//     id:"price_1M0pHCJjFKHFVjIvPcUevcQh",
+//     title: "Flag of Ukraine",
+//     price: 10.99
+// },
+// ]
 
-function getProductData(id) {
-    let productData = productsArray.find(product => product.id === id);
+// function getProductData(id) {
+//     let productData = productsArray.find(product => product.id === id);
 
-    if (productData == undefined) {
-        console.log("no product data for this ID:" + id)
-        return undefined;
-    }
+//     if (productData == undefined) {
+//         console.log("no product data for this ID:" + id)
+//         return undefined;
+//     }
 
-    return productData;
-}
+//     return productData;
+// }
 
-export {productsArray, getProductData};
+// export {productsArray, getProductData};
